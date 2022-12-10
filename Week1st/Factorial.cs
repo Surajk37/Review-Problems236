3a79e70460e31e2aab13f15f4cf68fa2b756462e
Surajk37 <surajkhot9993@gmail.com>
2022-12-10 20:56:18 +0530

    [Suraj] Calculating Factorial of Number

---
+++ Week1st/Factorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1st
+{
+    public class Factorial
+    {
+        public void CalFactorial()
+        {
+            int num, fact;
+            Console.WriteLine("Enter A Number");
+            num= Convert.ToInt32(Console.ReadLine());
+            fact = num;
+            for (int i = num -1 ; i >= 1; i--)
+            {
+                
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial :{0}", fact);
+
+        }
+    }
+}
---
+++ Week1st/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Week1st
+{
+    class Program
+    {
+       public static void Main()
+        {
+           Factorial factorial = new Factorial();
+            factorial.CalFactorial();
+        }
+    }
+}
